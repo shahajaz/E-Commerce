73e76b443eb1bc51a7c0dd08eac364e2cf2f7004
Mohammad Ajaz Shah <ajazmohammad2003@gmail.com>
2026-06-04 21:30:03 +0530

    Nav building

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -3,7 +3,27 @@ import { useTheme } from "../../contexts/ThemeContext";
 import { useDispatch, useSelector } from "react-redux";
 
 const Navbar = () => {
-  return <></>;
+
+  const { theme, toggleTheme } = useTheme();
+  
+  const dispatch = useDispatch();
+
+  const { cart } = useSelector((state) => state.cart);
+
+  let cartItemCount = 0;
+
+  if ( cart ){
+    cartItemCount = cart.reduce((total, item) => total + item.quantity, 0);
+  }
+
+  return <>
+
+  <nav className="fixed left-0 w-full top-0 z-50 bg-background/80 backdrop-blur-md border-b border-border">
+    <div className="max-w-7xl max-auto px-4">
+      
+    </div>
+  </nav>
+  </>;
 };
 
 export default Navbar;
