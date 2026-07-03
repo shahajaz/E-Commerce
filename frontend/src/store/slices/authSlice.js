a059c0a1d31ff31e626888e97807759a524f03af
Mohammad Ajaz Shah <ajazmohammad2003@gmail.com>
2026-07-03 22:04:08 +0530

    Auth Slice

--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -2,6 +2,29 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosInstance } from "../../lib/axios";
 import { toast } from "react-toastify";
 
+export const register = createAsyncThunk("auth/register", async(data, thunkAPI) => {
+  try {
+    const res = await axiosInstance.post("/auth/register", data);
+    return res.data;
+  }catch (error) {
+    toast.error("Registration failed");
+    return thunkAPI.rejectWithValue(error.response.data);
+  }
+
+});
+
+export const login = createAsyncThunk("auth/register", async(data, thunkAPI) => {
+
+});
+
+export const getUser = createAsyncThunk("auth/register", async(_, thunkAPI) => {
+
+});
+
+export const logout = createAsyncThunk("auth/register", async(data, thunkAPI) => {
+
+});
+
 const authSlice = createSlice({
   name: "auth",
   initialState: {
