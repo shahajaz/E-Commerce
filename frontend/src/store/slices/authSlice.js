9cd535ec54c039e07120f99364c3a783d99c7be8
Mohammad Ajaz Shah <ajazmohammad2003@gmail.com>
2026-07-06 22:22:29 +0530

    AuthSlice Forgot password

--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -1,28 +1,67 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosInstance } from "../../lib/axios";
 import { toast } from "react-toastify";
+import {toggleAuthPopup} from "./popupSlice";
 
 export const register = createAsyncThunk("auth/register", async(data, thunkAPI) => {
-  try {
+  try{
     const res = await axiosInstance.post("/auth/register", data);
-    return res.data;
-  }catch (error) {
-    toast.error("Registration failed");
-    return thunkAPI.rejectWithValue(error.response.data);
+    toast.success(res.data.message);
+    thunkAPI.dispatch(toggleAuthPopup());
+    return res.data.user;
   }
-
+  catch (error){
+    toast.error(error.response.data.message);
+    return thunkAPI.rejectWithValue(error.response.data.message);
+  }
 });
 
-export const login = createAsyncThunk("auth/register", async(data, thunkAPI) => {
-
+export const login = createAsyncThunk("auth/login", async(data, thunkAPI) => {
+  try{
+    const res = await axiosInstance.post("/auth/login", data);
+    toast.success(res.data.message);
+    thunkAPI.dispatch(toggleAuthPopup());
+    return res.data.user;
+  }
+  catch (error){
+    toast.error(error.response.data.message);
+    return thunkAPI.rejectWithValue(error.response.data.message);
+  }
 });
 
-export const getUser = createAsyncThunk("auth/register", async(_, thunkAPI) => {
-
+export const getUser = createAsyncThunk("auth/me", async(_, thunkAPI) => {
+  try{
+    const res = await axiosInstance.get("/auth/me");
+    return res.data.user;
+  }
+  catch (error){
+    toast.error(error.response.data.message);
+    return thunkAPI.rejectWithValue(error.response.data.message || "Something went wrong");
+  }
 });
 
-export const logout = createAsyncThunk("auth/register", async(data, thunkAPI) => {
+export const logout = createAsyncThunk("auth/logout", async(data, thunkAPI) => {
+  try{
+    const res = await axiosInstance.get("/auth/logout");
+    thunkAPI.dispatch(toggleAuthPopup());
+    return null;
+  }
+  catch (error){
+    toast.error(error.response.data.message);
+    return thunkAPI.rejectWithValue(error.response.data.message || "Something went wrong");
+  }
+});
 
+export const forgotPassword = createAsyncThunk("auth/forgot/password", async(email, thunkAPI) => {
+  try{
+    const res = await axiosInstance.post("/auth/forgot/password/forgot?frontendUrl=http://localhost:5173", { email });
+    toast.success(res.data.message);
+    return null;
+  }
+  catch (error){
+    toast.error(error.response.data.message);
+    return thunkAPI.rejectWithValue(error.response.data.message);
+  }
 });
 
 const authSlice = createSlice({
